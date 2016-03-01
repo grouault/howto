965a25fa3d8d53f997ef60d9017201b78e858463
rouaul_g <rouaul_g@451ca551-2b01-48a8-bb59-7062f3cb1b21>
2016-03-01 16:31:09 +0000

    commit

---
+++ java/bonita/how-to-groovy/how-to-call-json-api.groovy
@@ -0,0 +1,21 @@
+# Call JSon Rest API
+
+import static org.codehaus.groovy.runtime.DefaultGroovyMethods.*;
+import groovy.json.JsonSlurper;
+
+def types = [];
+URL url = new URL("http://localhost:8080/bonita/portal/resource/process/Leave%20request/7.1/API/extension/test/leaveReasons");
+InputStream urlStream = null;
+def jsonContent = "null";
+try {
+
+	urlStream = url.openStream();
+	BufferedReader reader = new BufferedReader(new InputStreamReader(urlStream));
+
+	
+} finally {
+	if (urlStream != null) {
+		urlStream.close();
+	}
+}
+return jsonContent;
