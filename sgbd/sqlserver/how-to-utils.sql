5e8bd14620f505c5f90ed946545185d266c74784
grouault <gildasrouault@gmail.com>
2017-01-06 22:54:04 +0100

    Update how-to-utils.sql

--- sgbd/sqlserver/how-to-utils.sql
+++ sgbd/sqlserver/how-to-utils.sql
@@ -1,3 +1,6 @@
 -- Descrit les options de la connexion au moteur
 -- Util pour le format de date pour les tests
 DBCC USEROPTIONS;
+
+-- Procédure stockées d'aide sur les tables
+exec sp_help ARTICLES
