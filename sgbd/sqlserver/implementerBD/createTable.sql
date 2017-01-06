97f7bb39dc04c17c65a7ee0a8b675a20abf80310
grouault <gildasrouault@gmail.com>
2017-01-06 18:13:13 +0100

    Update createTable.sql

--- sgbd/sqlserver/implementerBD/createTable.sql
+++ sgbd/sqlserver/implementerBD/createTable.sql
@@ -21,3 +21,25 @@ CREATE TABLE COMMANDES(
   taux_remise numeric(2,0),
   numero_cli int,
   etat_cde char(2));
+
+CREATE TABLE HISTO_FAC(
+  numero_fac int identity(1000,1) not null,
+  date_fac datetime,
+  numero_cde int,
+  montantht smallmoney,
+  etat_fac char(2),
+  constraint pk_histo_fac primary key(numero_fac)
+);
+
+GO
+CREATE TABLE LIGNES_CDE(
+  numero_cde int not null,
+  numero_lig int not null,
+  reference_art nvarchar(16) not null,
+  qte_cde int default 1
+);
+GO
+
+ALTER TABLE LIGNES_CDE
+  ADD CONSTRAINT pk_lignes
+  PRIMARY KEY NONCLUSTERED (numero_cde, numero_lig);
