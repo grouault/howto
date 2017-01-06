6aaa5dfc481fe0ef10f440f564921e99cdd6729d
grouault <gildasrouault@gmail.com>
2017-01-06 18:10:53 +0100

    Update createTable.sql

--- sgbd/sqlserver/implementerBD/createTable.sql
+++ sgbd/sqlserver/implementerBD/createTable.sql
@@ -8,3 +8,16 @@ CREATE TABLE ARTICLES(
 	CODE_CAT int
 );
 exec sp_help ARTICLES
+
+CREATE TABLE CATEGORIES(
+  code_cat int identity(100,1),
+  libelle_cat nvarchar(200)
+);
+
+GO
+CREATE TABLE COMMANDES(
+  numero_cde int identity(1350,1),
+  date_cde date,
+  taux_remise numeric(2,0),
+  numero_cli int,
+  etat_cde char(2));
