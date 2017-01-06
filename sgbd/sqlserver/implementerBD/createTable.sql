4308c36c8cb305a13dcf2df0e271923836e40d29
grouault <gildasrouault@gmail.com>
2017-01-06 18:13:42 +0100

    Update createTable.sql

--- sgbd/sqlserver/implementerBD/createTable.sql
+++ sgbd/sqlserver/implementerBD/createTable.sql
@@ -9,6 +9,11 @@ CREATE TABLE ARTICLES(
 );
 exec sp_help ARTICLES
 
+-- Ajout d'une colonne
+ALTER TABLE CLIENTS ADD CODEREP char(2) NOT NULL;
+-- Modification d'une colonne existante
+ALTER TABLE CLIENTS ALTER COLUMN TELEPHONE char(14) NOT NULL;
+
 CREATE TABLE CATEGORIES(
   code_cat int identity(100,1),
   libelle_cat nvarchar(200)
