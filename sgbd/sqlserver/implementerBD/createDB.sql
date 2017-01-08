1b86ad6fe861aa8acb18f53eee58f7f58421caf7
grouault <gildasrouault@gmail.com>
2017-01-08 12:07:09 +0100

    Update createDB.sql

--- sgbd/sqlserver/implementerBD/createDB.sql
+++ sgbd/sqlserver/implementerBD/createDB.sql
@@ -1,13 +1,13 @@
-CREATE DATABASE GESCOM
+CREATE DATABASE GESCOM_DEMO
 ON PRIMARY(
   name=gescom_data,
-  filename='C:\donnees\gescom_data.mdf',
+  filename='D:\datas\sqlserver\gescom_demo\gescom_data.mdf',
   size= 6MB,
   maxsize= 15MB,
   filegrowth= 1MB)
 LOG ON(
   name=gescom_log,
-  filename='C:\donnees\gescom_log.ldf',
+  filename='D:\datas\sqlserver\gescom_demo\gescom_log.ldf',
   size= 2MB,
   maxsize= 2Mb,
   filegrowth= 0MB
