7261380df2a69d035fe03e605481ede6e085369d
grouault <gildasrouault@gmail.com>
2017-01-15 16:55:21 +0100

    Update how_to_join.sql

--- sgbd/sqlserver/extractDatas/how_to_join.sql
+++ sgbd/sqlserver/extractDatas/how_to_join.sql
@@ -1,3 +1,6 @@
+-- =========================================
+-- Tableau des produits commandés par client
+-- =========================================
 select co.numero_cde as 'Numéro cde', CONVERT(char(10), co.date_cde, 103) as 'Date cde', cli.nom as 'Nom Client',
 	lco.reference_art as 'Réf. article', a.prixht as 'Prix HT', lco.qte_cde as 'Qtté commandée'
 	from CLIENTS cli 
