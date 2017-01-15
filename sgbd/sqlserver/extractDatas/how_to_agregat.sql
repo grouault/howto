9a9d0ee403c8e4f9780f3154e67213486ebded2a
grouault <gildasrouault@gmail.com>
2017-01-15 11:44:17 +0100

    Create how_to_agregat.sql

---
+++ sgbd/sqlserver/extractDatas/how_to_agregat.sql
@@ -0,0 +1,13 @@
+-- =========================================================================
+-- Distinct = Group by
+-- Distinct => au fur et à mesure que l'information est présentée
+-- Group by => Opération de tri, avec sous ensemble, on affiche l'étiquette
+--    ou les étiquettes si plusieurs champs dans le group by
+-- =========================================================================
+select distinct ville
+from clients;
+go
+
+select ville
+from clients
+group by ville;
