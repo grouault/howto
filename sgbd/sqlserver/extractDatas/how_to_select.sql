425a33395649e91662752ce354d36c213094ea0a
grouault <gildasrouault@gmail.com>
2017-01-15 10:55:52 +0100

    Update how_to_select.sql

--- sgbd/sqlserver/extractDatas/how_to_select.sql
+++ sgbd/sqlserver/extractDatas/how_to_select.sql
@@ -3,3 +3,14 @@
 -- ====================
 select 'référence'=reference, 'désignatin de l''article'=designation 
 from ARTICLES;
+
+-- ====================
+-- Restriction
+-- ====================
+Select nom, prenom, numero, ville
+FROM CLIENTS
+where ville = 'Nantes';
+
+Select nom, prenom, numero, ville
+FROM CLIENTS
+where codepostal between 44000 and 44999
