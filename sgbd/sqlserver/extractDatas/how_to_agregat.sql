5767fa1e666408539cb79041e710cc8087a609a2
grouault <gildasrouault@gmail.com>
2017-01-15 12:30:07 +0100

    Update how_to_agregat.sql

--- sgbd/sqlserver/extractDatas/how_to_agregat.sql
+++ sgbd/sqlserver/extractDatas/how_to_agregat.sql
@@ -18,3 +18,17 @@ select MIN(a.prixht) as 'Prix minimum',
 	AVG(a.prixht) as 'Prix moyen',
 	MAX(a.prixht) as 'Prix maximum'
 from articles a;
+
+-- =========================================================================
+-- Sortir le prix minimun et maximum sur la meme ligne
+-- =========================================================================
+select a1.prixht as 'Prix maximum', a1.reference, a2.prixht as 'Prix minimum', a2.reference
+ from articles a1 
+	left join articles a2 on a1.code_cat<>a2.code_cat 
+ where a1.prixht >=any(
+	select max(a2.prixht)
+	from ARTICLES a2
+ ) and a2.prixht <=any(
+	select min(a2.prixht)
+	from ARTICLES a2
+ );
