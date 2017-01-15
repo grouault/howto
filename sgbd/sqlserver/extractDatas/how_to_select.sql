a5c8f9157f60c32d423101d95c2381227a5a41f0
grouault <gildasrouault@gmail.com>
2017-01-15 11:42:28 +0100

    Update how_to_select.sql

--- sgbd/sqlserver/extractDatas/how_to_select.sql
+++ sgbd/sqlserver/extractDatas/how_to_select.sql
@@ -45,3 +45,17 @@ from ARTICLES a;
 select rtrim(cli.prenom) + ' ' +  rtrim(cli.nom) AS patronyme,
 	substring(cli.codepostal, 1, 2)
 	from CLIENTS cli;
+
+-- =========================================================================
+-- Distinct = Group by
+-- Distinct => au fur et à mesure que l'information est présentée
+-- Group by => Opération de tri, avec sous ensemble, on affiche l'étiquette
+--    ou les étiquettes si plusieurs champs dans le group by
+-- =========================================================================
+select distinct ville
+from clients;
+go
+
+select ville
+from clients
+group by ville;
