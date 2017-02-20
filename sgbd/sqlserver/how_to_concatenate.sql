7631a5f25e2c54521854bb6cebdb6b48720e356b
grouault <gildasrouault@gmail.com>
2017-02-20 22:50:37 +0100

    Update how_to_concatenate.sql

--- sgbd/sqlserver/how_to_concatenate.sql
+++ sgbd/sqlserver/how_to_concatenate.sql
@@ -1,1 +1,2 @@
-https://www.simple-talk.com/sql/t-sql-programming/concatenating-row-values-in-transact-sql/
+https://www.simple-talk.com/sql/t-sql-programming/concatenating-row-values-in-transact-sql
+http://stackoverflow.com/questions/31211506/how-stuff-and-for-xml-path-work-in-sql-server
