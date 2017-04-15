950bfdc26a11770eb110f9aad66386f0337e7897
grouault <gildasrouault@gmail.com>
2017-04-15 21:10:19 +0200

    Update ddl_users.sql

--- sgbd/oracle/users/ddl_users.sql
+++ sgbd/oracle/users/ddl_users.sql
@@ -1,2 +1,12 @@
+-- Création d'un user avec ses droits associés
 create user SURSAUD_DEV identified by SURSAUD_DEV
-DEFAULT TABLESPACE
+default tablespace SURSAUD
+temporary tablespace TEMP;
+
+GRANT resource to SURSAUD_DEV;
+GRANT CREATE_ANY_VIEW to SURSAUD_DEV;
+GRANT CREATE session to SURSAUD_DEV;
+GRANT DEBUG_ANY_PROCEDURE to SURSAUD_DEV;
+GRANT DEBUG CONNECT SESSION to SURSAUD_DEV;
+GRANT UNLIMITED TABLESPACE to SURSAUD_DEV;
+GRANT execute on dbms_lock to SURSAUD_DEV;
