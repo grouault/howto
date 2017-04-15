1eccb4036c469eca6a026af5e2c03d231e641df2
grouault <gildasrouault@gmail.com>
2017-04-15 20:33:20 +0200

    Update infos.sql

--- sgbd/oracle/tablespace/infos.sql
+++ sgbd/oracle/tablespace/infos.sql
@@ -1,16 +1,13 @@
+-- info sur Tablespace et Fichier de données
 select substr(d.name,1,50) "FICHIER", d.TS#
 from V$datafile d, V$tablespace t
 where d.TS#=t.TS#
 order by d.TS#;
 
+-- infos sur les tablespaces
 select s.TABLESPACE_NAME, s.STATUS, s.CONTENTS
 from SYS.DBA_TABLESPACES s;
 
-select tablespace_name
-from SYS.DBA_TABLESPACES;
-
-select *
-from SYS.DBA_TABLESPACES;
-
+-- infos sur les fichiers de données
 select *
 from SYS.DBA_DATA_FILES;
