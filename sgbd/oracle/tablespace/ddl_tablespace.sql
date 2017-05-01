0dd2bcd5caa1b25345a8a1347869195bf1a47b19
grouault <gildasrouault@gmail.com>
2017-05-01 12:24:06 +0200

    Update ddl_tablespace.sql

--- sgbd/oracle/tablespace/ddl_tablespace.sql
+++ sgbd/oracle/tablespace/ddl_tablespace.sql
@@ -1,10 +1,18 @@
--- création tablespace FILE
+-- création tablespace FILE : LINUX
 CREATE TABLESPACE SURSAUD
 DATAFILE '/ora01/app/oracle/oradata/ORA11G/SURSAUD_01.dbf' SIZE 100M
 AUTOEXTEND OFF
 EXTENT MANAGEMENT LOCAL UNIFORM SIZE 65K;
 
+-- WINDDOWS
+CREATE TABLESPACE SURSAUD
+DATAFILE 'd:/app/oracle/oradata/orcl/SURSAUD_01.dbf' SIZE 100M
+AUTOEXTEND OFF
+EXTENT MANAGEMENT LOCAL UNIFORM SIZE 65K;
+
+-------------------------------
 -- création tablespace INDEX
+-------------------------------
 CREATE TABLESPACE SURSAUD_IDX
 DATAFILE '/ora01/app/oracle/oradata/ORA11G/SURSAUD_IDX_01.dbf' SIZE 100M
 AUTOEXTEND OFF
