4239cf909cc285f61d3958e2bbd3bc3eda062fc9
grouault <gildasrouault@gmail.com>
2017-05-02 15:19:45 +0200

    Update procedures_base.sql

--- sgbd/oracle/Procedures/procedures_base.sql
+++ sgbd/oracle/Procedures/procedures_base.sql
@@ -1,4 +1,18 @@
+-- =====================
+-- INTEGATION CERTIFICAT
+-- =====================
+set serveroutput on;
+DECLARE
+
+BEGIN
+  DBMS_OUTPUT.ENABLE( 1000000 ) ;	
+  INTEG_DECES_CERTIFICAT.traite_deces_certificat(sysdate);
+  DBMS_OUTPUT.PUT_LINE('TEST OK');
+END;
+
+-- =========================
 -- Call procedure in Package.
+-- =========================
 set serveroutput on;
 DECLARE
 
