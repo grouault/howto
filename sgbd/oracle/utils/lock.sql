c75088225e8bde650394a27f93360018b266558d
grouault <gildasrouault@gmail.com>
2017-05-04 14:41:28 +0200

    Create lock.sql

---
+++ sgbd/oracle/utils/lock.sql
@@ -0,0 +1,23 @@
+--select locks
+select
+   c.owner,
+   c.object_name,
+   c.object_type,
+   b.sid,
+   b.serial#,
+   b.status,
+   b.osuser,
+   b.machine
+from
+   v$locked_object a ,
+   v$session b,
+   dba_objects c
+where
+   b.sid = a.session_id
+and
+   a.object_id = c.object_id;
+   
+--kill locks
+alter system kill session '33,11047' IMMEDIATE
+
+il faut que tu te connectes avec l’utilisateur system/manager
