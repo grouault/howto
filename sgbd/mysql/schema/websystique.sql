db3695d52ba1020f896857927e8d049ecd81d553
grouault <gildasrouault@gmail.com>
2017-06-15 21:14:22 +0200

    Create websystique.sql

--- sgbd/mysql/schema/websystique.sql
+++ sgbd/mysql/schema/websystique.sql
@@ -4,6 +4,7 @@
 CREATE USER 'websystique'@'localhost' IDENTIFIED BY 'websystique';
 GRANT USAGE ON * . * TO 'websystique'@'localhost' IDENTIFIED BY 'websystique';
 CREATE SCHEMA `websystique` DEFAULT CHARACTER SET utf8;
+GRANT ALL PRIVILEGES ON `websystique` . * TO 'websystique'@'localhost';
 
 create table APP_USER (
    id BIGINT NOT NULL AUTO_INCREMENT,
