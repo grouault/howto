a4cf841bcb30dacaf6b9a6d1ebaaf49ae73dc05b
grouault <gildasrouault@gmail.com>
2017-07-04 21:06:55 +0200

    Update exemple.js

--- javascript/angular.js/filter/exemple.js
+++ javascript/angular.js/filter/exemple.js
@@ -1,5 +1,17 @@
-    
-    module.filter("shouldShowAgence", function(){
+<select name="uagence" id="uagence"
+        class="form-control"
+        ng-options="agence.nom for agence in agences | filter:shouldShowAgence track by agence.id"
+        ng-model="form.agence">    
+
+// initialisation des champs a cacher.
+$scope.initializeChampCaches = function () {
+    $scope.selAntVisible = true; // champ de selection d'une antenne
+    if ($scope.form && 'CA' === $scope.form.role.nom) {
+        $scope.selAntVisible = false;
+    }
+};
+
+module.filter("shouldShowAgence", function(){
         return function(agences) {
             var filtered = [];
             console.log('test out');
