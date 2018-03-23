987bd9346976220632fc02d935140c0a1e73ece0
grouault <gildasrouault@gmail.com>
2018-03-23 16:31:05 +0100

    Update user_infos.sql

--- sgbd/mysql/users/user_infos.sql
+++ sgbd/mysql/users/user_infos.sql
@@ -5,5 +5,9 @@ select host, user, password from mysql.user;
 -- Create-user
 CREATE SCHEMA `salsa-lot2` DEFAULT CHARACTER SET utf8;
 CREATE USER 'salsa2'@'localhost' IDENTIFIED BY 'salsa2';
+-- donner des droits au user Salsa2
 GRANT USAGE ON * . * TO 'salsa2'@'localhost' IDENTIFIED BY 'salsa2';
 GRANT ALL PRIVILEGES ON `salsa-lot2` . * TO 'salsa2'@'localhost';
+-- donner des droits au user Salsa
+GRANT USAGE ON * . * TO 'salsa'@'localhost' IDENTIFIED BY 'salsa';
+GRANT ALL PRIVILEGES ON `salsa` . * TO 'salsa'@'localhost';
