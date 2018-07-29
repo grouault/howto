d0d15f99e0ec6051c016205a7aedf141ff7ee1d0
grouault <gildasrouault@gmail.com>
2018-07-29 09:25:17 +0200

    Update modifier-table.sql

--- sgbd/sqlserver/formation/implementer-bdd/modifier-table.sql
+++ sgbd/sqlserver/formation/implementer-bdd/modifier-table.sql
@@ -1,4 +1,11 @@
 - # modifier une table
 - modifier la structure de la table
+
+-- ajoute colonne
 ALTER TABLE CLIENTS 
   ADD CODEREP char(2) NOT NULL;
+  
+-- modifier colonne
+ALTER TABLE 
+  ATLER COLUMN telephone char(14) NOT NULL;
+  
