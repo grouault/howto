6fd32bbf875e29a8d19000ee1c9b329d3c9a2b17
grouault <gildasrouault@gmail.com>
2018-07-29 09:30:20 +0200

    Update identity.sql

--- sgbd/sqlserver/formation/implementer-bdd/identity.sql
+++ sgbd/sqlserver/formation/implementer-bdd/identity.sql
@@ -1,5 +1,7 @@
 -- # type identity
+
 -- séquence : on peut avoir des absences dans la séquences
+-- ==> spécification compteur
 CREATE TABLE CATEGORIES(
   code_cat int identity(100,1);
   libelle_cat nvarchar(200)
