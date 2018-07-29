975852a33cf9d24d590efd8629b54357061e00d7
grouault <gildasrouault@gmail.com>
2018-07-29 09:29:10 +0200

    Update identity.sql

--- sgbd/sqlserver/formation/implementer-bdd/identity.sql
+++ sgbd/sqlserver/formation/implementer-bdd/identity.sql
@@ -4,3 +4,6 @@ CREATE TABLE CATEGORIES(
   code_cat int identity(100,1);
   libelle_cat nvarchar(200)
 );
+
+-- pour cette colonne, on ne fournit jamais de valeur explicit
+-- sqlserver fournit un code catégorie : commence à 1, incrémentation par pas de 1
