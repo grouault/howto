f9081652af416ab9049ae309937249b58c2f69b0
grouault <gildasrouault@gmail.com>
2018-07-29 08:55:38 +0200

    Update modifier.sql

--- sgbd/sqlserver/formation/implementer-bdd/modifier.sql
+++ sgbd/sqlserver/formation/implementer-bdd/modifier.sql
@@ -1,2 +1,5 @@
+-- modifier un certaines nombre de paramètres
+
+-- modifier l'espace disque
 -- modifier la taille des fichiers de données
 ALTER DATABASE GESCOM MODIFY FILE (name=gescom_data, size= 20MB);
