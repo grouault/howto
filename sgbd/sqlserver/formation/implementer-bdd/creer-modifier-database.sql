98051c65ba361da11f69d4fe3be68e235c7a7bb4
grouault <gildasrouault@gmail.com>
2018-07-30 17:35:16 +0200

    Update creer-modifier-database.sql

--- sgbd/sqlserver/formation/implementer-bdd/creer-modifier-database.sql
+++ sgbd/sqlserver/formation/implementer-bdd/creer-modifier-database.sql
@@ -15,3 +15,18 @@ objet de base de données : create / alter / drop
 -- # créer à partir d'un script
 Collate : ordre de tri utilisé 
 exemple French_CI_AI : tri suivant ordre alpahabétique francais, case insentivive, accent insensitive
+
+-- # MODIFIER
+-- modifier un certaines nombre de paramètres
+
+-- modifier l'espace disque
+-- modifier la taille des fichiers de données
+ALTER DATABASE GESCOM MODIFY FILE (name=gescom_data, size= 20MB);
+-- ==> taille initiale>max_size : pas trop grave car lu quand on veut agrandir le fichier de données.
+
+-- ajouter un fichier
+ALTER DATABASE 
+ADD FILE(
+    name=gescom_data2, 
+    filename='c:\donnees\gescom_data2.ndf',
+    size=20MB);
