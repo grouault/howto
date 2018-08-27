f9c20d83533a697a07030bbfdffe356ea16073f5
grouault <gildasrouault@gmail.com>
2018-08-27 08:51:33 +0200

    Update app.module.ts

--- javascript/angular/angular-cli/structure/app.module.ts
+++ javascript/angular/angular-cli/structure/app.module.ts
@@ -1,3 +1,25 @@
-# src/app
-app.module.ts : 
-@NgModule({ ... }) ==> boostrap : 
+/*
+src/app
+app.module.ts
+Module principale de l'application
+*/
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+
+import { AppComponent } from './app.component';
+import { MonComposantComponent } from './components/mon-composant/mon-composant.component'
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    MonComposantComponent
+  ],
+  imports: [
+    BrowserModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent] /*  qu'est ce que je dois bootstraper / démarrer pour initialiser le module */
+})
+export class AppModule { }
+
