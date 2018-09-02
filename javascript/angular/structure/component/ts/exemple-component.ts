0deeb1cd744c047ac901b7c97587080660895168
grouault <gildasrouault@gmail.com>
2018-09-02 16:51:50 +0200

    Create exemple-component.ts

---
+++ javascript/angular/structure/component/ts/exemple-component.ts
@@ -0,0 +1,17 @@
+import {Component} from "@angular/core";
+/**
+ * Created by grouault on 26/08/2018.
+ */
+
+@Component({
+  selector: 'app-mon-composant',
+  templateUrl: './mon-composant.component.html',
+  styleUrls: ['./mon-composant.component.css']
+})
+export class MonComposantComponent {
+
+   private title: string = 'Mon composant';
+
+  constructor(){}
+
+}
