f8f62785db6745d7d913b8ead58ac0b0af047cba
grouault <gildasrouault@gmail.com>
2018-09-08 21:49:59 +0200

    Create films.ts

---
+++ javascript/angular/structure/modele/exemples/films.ts
@@ -0,0 +1,13 @@
+export class Film{
+
+  public title: string = '';
+  public director: string = '';
+  public episode_id: string= '';
+
+  constructor(title: string, director: string, episodeId: string) {
+      this.title = title;
+      this.director = director;
+      this.episode_id = episodeId;
+  }
+
+}
