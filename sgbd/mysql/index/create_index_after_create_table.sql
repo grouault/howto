1ea651dca1cf1e9b51e738d2d69bec26d6233b6f
grouault <gildasrouault@gmail.com>
2018-10-27 13:18:38 +0200

    Update create_index_after_create_table.sql

--- sgbd/mysql/index/create_index_after_create_table.sql
+++ sgbd/mysql/index/create_index_after_create_table.sql
@@ -5,16 +5,19 @@ ADD INDEX [nom_index] (colonne_index [, colonne2_index ...]); --Ajout d'un index
 ALTER TABLE nom_table
 ADD UNIQUE [nom_index] (colonne_index [, colonne2_index ...]); --Ajout d'un index UNIQUE
 
-ALTER TABLE nom_table
-ADD FULLTEXT [nom_index] (colonne_index [, colonne2_index ...]); --Ajout d'un index FULLTEXT
 
+ALTER TABLE nom_table
+ADD CONSTRAINT [symbole_contrainte] UNIQUE ind_uni_col2 (colonne2);  -- UNIQUE avec constraint
+                                       
+ALTER TABLE nom_table
+ADD FULLTEXT [nom_index] (colonne_index [, colonne2_index ...]); --Ajout d'un index FULLTEXT                                       
+                                         
 -- CREATE_INDEX
 CREATE INDEX nom_index
 ON nom_table (colonne_index [, colonne2_index ...]);  -- Crée un index simple
 
 CREATE UNIQUE INDEX nom_index
 ON nom_table (colonne_index [, colonne2_index ...]);  -- Crée un index UNIQUE
-
 
 CREATE FULLTEXT INDEX nom_index
 ON nom_table (colonne_index [, colonne2_index ...]);  -- Crée un index FULLTEXT
