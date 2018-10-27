3fca804cb8ccdb02fff5f931052d2c7e83590a22
grouault <gildasrouault@gmail.com>
2018-10-27 13:02:56 +0200

    Update create_index.sql

--- sgbd/mysql/index/create_index.sql
+++ sgbd/mysql/index/create_index.sql
@@ -39,3 +39,18 @@ CREATE TABLE nom_table (
     FULLTEXT [INDEX] ind_full_col3 (colonne3)   -- Crée un index FULLTEXT sur la colonne3, INDEX est facultatif
 )
 ENGINE=MyISAM;                   
+
+-- index sur plusieurs attributs
+CREATE TABLE Animal (
+    id SMALLINT UNSIGNED NOT NULL AUTO_INCREMENT,
+    espece VARCHAR(40) NOT NULL,
+    sexe CHAR(1),
+    date_naissance DATETIME NOT NULL,
+    nom VARCHAR(30),
+    commentaires TEXT,
+    PRIMARY KEY (id),
+    INDEX ind_date_naissance (date_naissance),  
+    INDEX ind_nom (nom(10)),                    
+    UNIQUE INDEX ind_uni_nom_espece (nom, espece)  -- Index sur le nom et l'espece
+)
+ENGINE=INNODB;
