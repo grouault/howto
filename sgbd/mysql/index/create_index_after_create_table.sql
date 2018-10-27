391d49d44c2e93504d4613e6b1ebd0c33ed882e9
grouault <gildasrouault@gmail.com>
2018-10-27 13:12:03 +0200

    Update create_index_after_create_table.sql

--- sgbd/mysql/index/create_index_after_create_table.sql
+++ sgbd/mysql/index/create_index_after_create_table.sql
@@ -1,3 +1,4 @@
+-- ALTER_TABLE
 ALTER TABLE nom_table
 ADD INDEX [nom_index] (colonne_index [, colonne2_index ...]); --Ajout d'un index simple
 
@@ -6,3 +7,14 @@ ADD UNIQUE [nom_index] (colonne_index [, colonne2_index ...]); --Ajout d'un inde
 
 ALTER TABLE nom_table
 ADD FULLTEXT [nom_index] (colonne_index [, colonne2_index ...]); --Ajout d'un index FULLTEXT
+
+-- CREATE_INDEX
+CREATE INDEX nom_index
+ON nom_table (colonne_index [, colonne2_index ...]);  -- Crée un index simple
+
+CREATE UNIQUE INDEX nom_index
+ON nom_table (colonne_index [, colonne2_index ...]);  -- Crée un index UNIQUE
+
+
+CREATE FULLTEXT INDEX nom_index
+ON nom_table (colonne_index [, colonne2_index ...]);  -- Crée un index FULLTEXT
