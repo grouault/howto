e3c1d1ab8672f79b0d3a03f32408b15820dd3339
grouault <gildasrouault@gmail.com>
2018-10-27 12:53:17 +0200

    Create create_index.sql

---
+++ sgbd/notes/index/mysql/create_index.sql
@@ -0,0 +1,4 @@
+CREATE TABLE nom_table (
+    colonne1 INT KEY,                  -- Crée un index simple sur colonne1
+    colonne2 VARCHAR(40) UNIQUE,       -- Crée un index unique sur colonne2
+);
