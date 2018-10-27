8eb58d356349dd2038c62cfcc5bdf9805e5bada6
grouault <gildasrouault@gmail.com>
2018-10-27 12:55:10 +0200

    Delete create_index.sql

--- sgbd/notes/index/mysql/create_index.sql
+++
@@ -1,4 +0,0 @@
-CREATE TABLE nom_table (
-    colonne1 INT KEY,                  -- Crée un index simple sur colonne1
-    colonne2 VARCHAR(40) UNIQUE,       -- Crée un index unique sur colonne2
-);
