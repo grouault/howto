d31a83a516c673059ae8c16199f8c4fe31fe5e13
grouault <gildasrouault@gmail.com>
2018-11-01 19:09:18 +0100

    Update 1-selection.sql

--- sgbd/sqlserver/formation/extraire-donnees/1-selection.sql
+++ sgbd/sqlserver/formation/extraire-donnees/1-selection.sql
@@ -1,3 +1,14 @@
 --
 -- Selectionner une ou plusieurs colonnes
+-- Opérateur Select auquel 
 --
+
+-- 0- like
+-- % : remplace 0 à N caractères
+-- _ : remplace 1 caractère
+where (ville like 'N%')
+
+-- 1- Critère de selection Null
+-- NULL = Absence de saisie.
+-- Toute comparaison avec Null est faux.
+where ville IS NULL
