5ecf0977da67d33350d1dcf160a85f73014c4f11
grouault <gildasrouault@gmail.com>
2018-11-01 19:12:30 +0100

    Update 1-selection.sql

--- sgbd/sqlserver/formation/extraire-donnees/1-selection.sql
+++ sgbd/sqlserver/formation/extraire-donnees/1-selection.sql
@@ -7,8 +7,15 @@
 -- % : remplace 0 à N caractères
 -- _ : remplace 1 caractère
 where (ville like 'N%')
+where (prenom like 'NO_m%')
 
 -- 1- Critère de selection Null
 -- NULL = Absence de saisie.
 -- Toute comparaison avec Null est faux.
 where ville IS NULL
+
+-- 2- Date
+-- extraire une partie de la date
+where DATEPART (mm, date_cde) = 6
+-- difference
+where DATEDIFF(year, GETDATE(), date_cde) < 3
