282cb958b395f312e736946bf9b037f436ffd1a1
grouault <gildasrouault@gmail.com>
2018-11-12 14:17:34 +0100

    Update how_to_insert.sql

--- sgbd/mysql/crud/how_to_insert.sql
+++ sgbd/mysql/crud/how_to_insert.sql
@@ -1,2 +1,10 @@
+-- exemple
 INSERT INTO Animal 
 VALUES (1, 'chien', 'M', '2010-04-05 13:43:00', 'Rox', 'Mordille beaucoup');
+
+--
+INSERT INTO PROFIL_PRESTATAIRE VALUES
+(1, 'DEBUTANT', 'débutant'),
+(2, 'NORMAL', 'normal'),
+(3, 'CONFIRME', 'confirmé'),
+(2, 'EXPERT', 'expert');
