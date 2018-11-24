ec92bbffb2c95921e6749a7d07890a1cc63b5c33
grouault <gildasrouault@gmail.com>
2018-11-24 18:56:44 +0100

    Create job.sql

---
+++ sgbd/administration/job.sql
@@ -0,0 +1,6 @@
+--
+-- scriptint Job
+--
+-- ==> permet de faire un script de sauvegarde des jobs
+-- les jobs sont associés à l'instance et pas à une base
+-- ssms : view > object Explorer Detail > ==> permet d'accéder au scripting
