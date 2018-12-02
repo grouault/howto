3f570e32c37147e3207929d57bbca38c15f74c58
grouault <gildasrouault@gmail.com>
2018-12-02 16:36:03 +0100

    Update 2-jointures.sql

--- sgbd/sqlserver/formation/extraire-donnees/2-jointures.sql
+++ sgbd/sqlserver/formation/extraire-donnees/2-jointures.sql
@@ -1,6 +1,14 @@
+-- =================
 -- Produit Cartésien
+-- =================
 -- Opérateur compétitif :
 -- une table avec une seule ligne d'information : on veut rajouter cette information sur toutes les lignes d'une autre table.
 -- Ecriture normalisé : cross join
+select c.libelle, a.designation
+from [dbo].[CATEGORIES] c cross join [dbo].[ARTICLES] a;
+
+-- ========
+-- Jointure
+-- ========
 select c.libelle, a.designation
 from [dbo].[CATEGORIES] c cross join [dbo].[ARTICLES] a;
