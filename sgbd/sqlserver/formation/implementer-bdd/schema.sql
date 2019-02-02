4b39a48302a752dd5337aa128da3d220dbc8928b
grouault <gildasrouault@gmail.com>
2019-02-02 12:51:26 +0100

    Update schema.sql

--- sgbd/sqlserver/formation/implementer-bdd/schema.sql
+++ sgbd/sqlserver/formation/implementer-bdd/schema.sql
@@ -2,4 +2,6 @@
 -- sql-server propose un niveau intermédiaire d'orgnaisation entre la base de données et les tables
 -- appelé schéma
 -- schéma unique par défaut : dbo (data base ownner)
+-- constituer des ensemble logiques qui organise logiquement les tables pour donner un sens supplémentaire, 
+-- un niveau intermédiare de compréhension de ces tables
 --
