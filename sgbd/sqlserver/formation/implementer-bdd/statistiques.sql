69b9ebbeddcaec7c5e69806236966f435e9b6a52
grouault <gildasrouault@gmail.com>
2019-02-02 12:42:42 +0100

    Update statistiques.sql

--- sgbd/sqlserver/formation/implementer-bdd/statistiques.sql
+++ sgbd/sqlserver/formation/implementer-bdd/statistiques.sql
@@ -5,5 +5,6 @@
 -- tient compte de tout un tas d'élément : le jours, l'heure et autres paramètres que l'on connaît tel que la densité du traffic
 -- En fonction des différents paramètres, on choisit le chemin optimum
 -- Pour sql-server c'est la même chose, si on met en place des index, mais si on ne lui donne pas la pertinence des index, 
--- qu'est ce qu'il va pouvoir gagner comme temps d'exécution
+-- qu'est ce qu'il va pouvoir gagner comme temps d'exécution ==> comme si on met à disposition une route sans connaître la destination,
+-- les différentes sorties, le nombre de voies, la largeur ==> incidence sur la vitesse à laquelle on peut se déplacer sur cette route
 --
