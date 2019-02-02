d9f8a55b2ebb1442d0a356fb5d665f8dab00c99b
grouault <gildasrouault@gmail.com>
2019-02-02 12:33:09 +0100

    Create reconstruire-index.sql

---
+++ sgbd/sqlserver/formation/implementer-bdd/reconstruire-index.sql
@@ -0,0 +1,21 @@
+--
+-- index sous forme d'arbre balancé est équitablement mis en place
+-- structure optimum au début de la vie de la table se déséquilibre avec le temps (plus suffisament d'espace dans une branche ou l'autre 
+-- de l'arbre) - sqlserver va rajouter un niveau intermédiaire pour pouvoir tjrs indexer la totalité des infos de la table
+-- ==> nécessité de reconstruire l'index pour garantir que l'arbre soit toujours corretement équilibré
+--
+
+--
+-- on ne casse pas la structure mais on la rééquilibre
+-- FILLFACTOR :
+--
+ALTER INDEX ALL
+  ON CLIENTS
+  REBUILD WITH (FILLFACTOR=50);
+
+--
+--
+--
+ALTER INDEX I_LIGNES_REFART
+  ON LIGNES_CDE
+  REBUILD WITH (PAD_INDEX=ON,FILLFACTOR=50);
