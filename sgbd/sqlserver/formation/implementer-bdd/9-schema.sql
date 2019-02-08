23b0360fc3429d158073bfb47a984f0fe4afe066
grouault <gildasrouault@gmail.com>
2019-02-08 17:58:15 +0100

    Update 9-schema.sql

--- sgbd/sqlserver/formation/implementer-bdd/9-schema.sql
+++ sgbd/sqlserver/formation/implementer-bdd/9-schema.sql
@@ -5,3 +5,5 @@
 -- constituer des ensemble logiques qui organise logiquement les tables pour donner un sens supplémentaire, 
 -- un niveau intermédiare de compréhension de ces tables
 --
+CREATE SCHEMA LIVRE;
+CREATE TABLE LIVRE.AUTEUR(int id, varchar nom);
