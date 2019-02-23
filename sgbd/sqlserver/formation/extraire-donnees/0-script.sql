1a905626d76d704796bb8236c1a4fbd59778f888
grouault <gildasrouault@gmail.com>
2019-02-23 11:34:45 +0100

    Update 0-script.sql

--- sgbd/sqlserver/formation/extraire-donnees/0-script.sql
+++ sgbd/sqlserver/formation/extraire-donnees/0-script.sql
@@ -38,4 +38,14 @@ SELECT REFERENCE_ART,
 GO
 SELECT RTRIM(nom)+' '+RTRIM(prenom) AS Patronyme,
 	SUBSTRING(codepostal,1,2) AS DEPARTEMENT
+-- ======================== --
+-- == AGREGAT == --
+-- ======================== --	
+SELECT DISTINCT ville FROM CLIENTS;
+-- OU
+SELECT ville FROM CLIENTS GROUP BY ville;
+GO
+SELECT ville, codepostal 
+	FROM CLIENTS
+	GROUP BY ville, codepostal;	
 	FROM CLIENTS;
