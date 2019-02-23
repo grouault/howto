c8bea83d5edefeffdb60c8071917ab00f1372f79
grouault <gildasrouault@gmail.com>
2019-02-23 11:02:26 +0100

    Update 0-script.sql

--- sgbd/sqlserver/formation/extraire-donnees/0-script.sql
+++ sgbd/sqlserver/formation/extraire-donnees/0-script.sql
@@ -8,3 +8,23 @@ GO
 SELECT Catégorie=CODE_CAT, 
        'Désignation de l''article'=DESIGNATION_ART 
        FROM ARTICLES;
+-- ================= --
+-- == RESTRICTION == --
+-- ================= --
+SELECT numero, nom, prenom, ville
+  FROM CLIENTS
+  WHERE ville='NANTES';
+GO
+SELECT numero, nom, prenom, ville
+  FROM CLIENTS
+  WHERE CODEPOSTAL BETWEEN 44000 AND 44999;
+GO
+SELECT numero, nom, prenom, ville
+  FROM CLIENTS
+  WHERE (ville like 'N%' OR ville IS NULL)
+    AND prenom like 'No_m%';
+GO
+SELECT numero_cde, numero_cli
+  FROM COMMANDES
+  WHERE DATEPART(mm, date_cde)=6
+    AND DATEDIFF(year, GETDATE(), date_cde)<3;	
