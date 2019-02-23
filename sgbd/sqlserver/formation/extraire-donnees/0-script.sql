eeb28ffd88c1656b51c1ba70a78016311138f2a7
grouault <gildasrouault@gmail.com>
2019-02-23 11:31:15 +0100

    Update 0-script.sql

--- sgbd/sqlserver/formation/extraire-donnees/0-script.sql
+++ sgbd/sqlserver/formation/extraire-donnees/0-script.sql
@@ -28,3 +28,14 @@ SELECT numero_cde, numero_cli
   FROM COMMANDES
   WHERE DATEPART(mm, date_cde)=6
     AND DATEDIFF(year, GETDATE(), date_cde)<3;	
+-- ======================== --
+-- == CALCUL ELEMENTAIRE == --
+-- ======================== --
+SELECT REFERENCE_ART,
+	'Ancien Prix'=PRIXHT_ART,
+	'Nouveau Prix'=PRIXHT_ART*1.1
+	FROM ARTICLES;
+GO
+SELECT RTRIM(nom)+' '+RTRIM(prenom) AS Patronyme,
+	SUBSTRING(codepostal,1,2) AS DEPARTEMENT
+	FROM CLIENTS;
