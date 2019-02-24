15489582676ee4e118af75d0ba57303caf1f56dc
grouault <gildasrouault@gmail.com>
2019-02-24 16:21:29 +0100

    Update 0-script-jointures.sql

--- sgbd/sqlserver/formation/ch3-extraire-donnees/0-script-jointures.sql
+++ sgbd/sqlserver/formation/ch3-extraire-donnees/0-script-jointures.sql
@@ -59,3 +59,34 @@ SELECT nom, prenom, adresse, codepostal, ville
   FROM CLIENTS
   ORDER BY nom, prenom, codepostal, ville
   OFFSET 10 ROWS FETCH NEXT 5 ROWS ONLY;
+
+
+-- =======================
+-- Opérateurs-ensembliste
+-- =======================
+-- UNION
+SELECT origine='Stock',convert(char(6), depot) as "Commande/dépot",
+       reference_art, qte_stk
+   FROM STOCKS
+UNION
+SELECT 'Commande', convert(char(6), cde.numero_cde), 
+       reference_art, qte_cde
+  FROM LIGNES_CDE lig INNER JOIN COMMANDES cde
+  ON lig.numero_cde=cde.numero_cde
+ORDER BY reference_art, origine;
+GO
+-- INTERSECTION
+SELECT *
+  FROM CLIENTS
+  WHERE ville LIKE 'Nantes%'
+INTERSECT
+SELECT *
+  FROM CLIENTS
+  WHERE codepostal BETWEEN 44000 and 44999;
+GO
+SELECT nom, prenom
+  FROM CLIENTS
+EXCEPT
+SELECT nom, prenom
+  FROM CLIENTS
+  WHERE codepostal BETWEEN 44000 and 44999;
