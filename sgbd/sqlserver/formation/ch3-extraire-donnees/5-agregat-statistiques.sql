bfb0c64143733e4c7d10b7664370e81890cd8356
grouault <gildasrouault@gmail.com>
2019-02-24 18:22:54 +0100

    Update 5-agregat-statistiques.sql

--- sgbd/sqlserver/formation/ch3-extraire-donnees/5-agregat-statistiques.sql
+++ sgbd/sqlserver/formation/ch3-extraire-donnees/5-agregat-statistiques.sql
@@ -1,6 +1,9 @@
 -- ===============================
 -- AGREGAT ET NIVEAU DE PRECISION
 -- ===============================
+-- Enrichir le jeu d'enregistrement avec les instructions
+-- with rollup
+-- with cube
 -- afficher pour un dépôt donné, un libellé_cat, une désignation_art la somme des quantité en stock
 -- ==> pratique mais on ne sait pas au total 
 ---- combien on dispose de clavier
