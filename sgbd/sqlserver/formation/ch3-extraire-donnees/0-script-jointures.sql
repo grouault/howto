d1ec5cf8ed8019f7568472e9411da780fdd13cd8
grouault <gildasrouault@gmail.com>
2019-02-24 17:45:48 +0100

    Update 0-script-jointures.sql

--- sgbd/sqlserver/formation/ch3-extraire-donnees/0-script-jointures.sql
+++ sgbd/sqlserver/formation/ch3-extraire-donnees/0-script-jointures.sql
@@ -173,10 +173,7 @@ SELECT * FROM CLI44;
 -- CALCUL AGREGAT ET NIVEAU DE PRECISION
 -- =====================================  
 -- afficher pour un dépôt donné, un libellé_cat, une désignation_art la somme des quantité en stock
--- ==> pratique mais on ne sait pas au total 
----- combien on dispose de clavier
----- combien on a d'articles dans le dépôt de Nantes
-----
+--
 SELECT depot, libelle_cat , designation_art ,sum(qte_stk) as total
   FROM STOCKS stk
   INNER JOIN ARTICLES art
