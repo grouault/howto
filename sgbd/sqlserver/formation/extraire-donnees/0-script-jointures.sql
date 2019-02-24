43650417bda3dab5fa55f7e0a5b1362cf81f2e3b
grouault <gildasrouault@gmail.com>
2019-02-24 12:30:06 +0100

    Update 0-script-jointures.sql

--- sgbd/sqlserver/formation/extraire-donnees/0-script-jointures.sql
+++ sgbd/sqlserver/formation/extraire-donnees/0-script-jointures.sql
@@ -18,6 +18,11 @@ SELECT CAT.CODE_CAT, CAT.LIBELLE_CAT, ART.DESIGNATION_ART
 	FROM CATEGORIES CAT INNER JOIN ARTICLES ART
 	ON ART.CODE_CAT=CAT.CODE_CAT;
 GO
+--
+-- Afficher par clients :
+-- le liste des articles commandés
+-- info: date_cde, Nom_client, article, prix_ht, qte_cde
+-- 
 SELECT CDE.numero_cde,
 	CONVERT(char(10), date_cde,103) as DateCde,
 	SUBSTRING(nom,1,10) Nom,
