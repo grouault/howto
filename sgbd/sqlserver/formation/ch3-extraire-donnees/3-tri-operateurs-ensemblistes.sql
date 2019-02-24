5066076baf23dc0fffeb4eb73b482364cf6ce37b
grouault <gildasrouault@gmail.com>
2019-02-24 16:11:02 +0100

    Update 3-tri-operateurs-ensemblistes.sql

--- sgbd/sqlserver/formation/ch3-extraire-donnees/3-tri-operateurs-ensemblistes.sql
+++ sgbd/sqlserver/formation/ch3-extraire-donnees/3-tri-operateurs-ensemblistes.sql
@@ -1,3 +1,15 @@
 -- 
--- 
+-- TRI
 --
+-- sql-server qui va décider de l'ordre d'affichage des infos.
+-- non garantit d'une exécution à l'autre de la requête
+-- si sql fait un parcours de la table sous-jacente à la requête, sql-server organise physiquement les données de la table 
+-- grace à l'index sous-jacent qui est index clustered 
+-- ou utilisation d'un index
+-- D'une éxecution à une autre de la requête, il y a eu peut-être mis à jour de cet index qui permet de tirer profit de cet index
+-- ayant pour incidence de réorganiser les données 
+-- ==> l'ordre des résultats peut-être alors modifier
+--
+-- Pour pallier, cela, il faut utiliser l'instruction ORDER BY
+--
+
