769e595e3288951acc79c8eb67af2cc64afada02
grouault <gildasrouault@gmail.com>
2019-02-24 17:19:42 +0100

    Update 4-tables-temporaires.sql

--- sgbd/sqlserver/formation/ch3-extraire-donnees/4-tables-temporaires.sql
+++ sgbd/sqlserver/formation/ch3-extraire-donnees/4-tables-temporaires.sql
@@ -8,6 +8,17 @@ SELECT numero, nom, prenom
   INTO CLINANTES
   FROM CLIENTS
   WHERE ville='Nantes';
-  
+--
+-- Table temporaire Globale : ##nom_table
+-- il faut la supprimer quand on en a plus besoin
+SELECT reference_art, sum(qte_cde) as quantite
+  INTO ##artCde
+  FROM LIGNES_CDE
+  GROUP BY reference_art;
+-- 
+-- Table temporaire locale : #nom_table
+-- locale à la connexion
+-- table non visible d'une autre connexion
+--
   
   
