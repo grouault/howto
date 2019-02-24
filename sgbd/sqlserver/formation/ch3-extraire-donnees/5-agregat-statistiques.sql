157b168f78c4197cd90371024342258db83f4e21
grouault <gildasrouault@gmail.com>
2019-02-24 18:20:48 +0100

    Update 5-agregat-statistiques.sql

--- sgbd/sqlserver/formation/ch3-extraire-donnees/5-agregat-statistiques.sql
+++ sgbd/sqlserver/formation/ch3-extraire-donnees/5-agregat-statistiques.sql
@@ -18,6 +18,8 @@ SELECT depot, libelle_cat , designation_art ,sum(qte_stk) as total
 -- with rollup
 -- ===============================
 -- permet de faire des sous-totaux par groupe d'agrégats
+-- 1- calcul agrégat normal
+-- 2- sous-total par groupe d'agrégats de droite à gauche
 -- group-by a1, a2, a3
 -- sous-total 1 : a1, a2, a3
 -- sous-total 2 : a1, a2
@@ -29,6 +31,18 @@ SELECT depot, libelle_cat , designation_art ,sum(qte_stk) as total
   WITH ROLLUP;
  -- mais ne permet pas de dire :
  ---- tout dépôt confondu pour les souris combien j'en ai en stock
- ---- tout dépôt confondu pour les souris pour logitech G500 combien j'en ai en stock
+ ---- tout dépôt confondu, par catégorie, pour les souris pour logitech G500 combien j'en ai en stock
  ---- tout dépôt confondu toute catégories confondues pour logitech G500, combien j'en ai en stock
-  
+ -- ===============================
+-- with cube
+-- ================================
+-- permet de faire des sous-totaux par groupe d'agrégats
+-- 1- calcul agrégat normal
+-- 2- sous-total par groupe d'agrégats de gauche à droite
+-- group-by a1, a2, a3
+-- sous-total 1 : a1, a2, a3
+-- sous-total 2 : a2,a3
+-- sous-total 3 : a3
+  ...
+  GROUP BY depot, libelle_cat , designation_art
+  WITH CUBE;
