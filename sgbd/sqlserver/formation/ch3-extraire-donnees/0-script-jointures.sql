24bc39706c8068147c27a7266efef9edb52d6382
grouault <gildasrouault@gmail.com>
2019-02-24 17:39:04 +0100

    Update 0-script-jointures.sql

--- sgbd/sqlserver/formation/ch3-extraire-donnees/0-script-jointures.sql
+++ sgbd/sqlserver/formation/ch3-extraire-donnees/0-script-jointures.sql
@@ -168,3 +168,35 @@ WITH CLI44 AS(
 SELECT COUNT(*) FROM CLIENTS ;
 
 SELECT * FROM CLI44;
+
+-- =====================================
+-- CALCUL AGREGAT ET NIVEAU DE PRECISION
+-- =====================================  
+SELECT depot, libelle_cat , designation_art ,sum(qte_stk) as total
+  FROM STOCKS stk
+  INNER JOIN ARTICLES art
+  ON stk.reference_art=art.reference_art
+  INNER JOIN CATEGORIES cat
+  ON cat.code_cat=art.code_cat
+  WHERE depot IN ('N','P1')
+  GROUP BY depot, libelle_cat , designation_art;
+GO
+SELECT depot, libelle_cat , designation_art ,sum(qte_stk) as total
+  FROM STOCKS stk
+  INNER JOIN ARTICLES art
+  ON stk.reference_art=art.reference_art
+  INNER JOIN CATEGORIES cat
+  ON cat.code_cat=art.code_cat
+  WHERE depot IN ('N','P1')
+  GROUP BY depot, libelle_cat , designation_art
+  WITH ROLLUP;
+GO
+SELECT depot, libelle_cat , designation_art ,sum(qte_stk) as total
+  FROM STOCKS stk
+  INNER JOIN ARTICLES art
+  ON stk.reference_art=art.reference_art
+  INNER JOIN CATEGORIES cat
+  ON cat.code_cat=art.code_cat
+  WHERE depot IN ('N','P1')
+  GROUP BY depot, libelle_cat , designation_art
+  WITH CUBE;
