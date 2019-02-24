2537d5c251e7f13ffebe5a2582f2688a3c791887
grouault <gildasrouault@gmail.com>
2019-02-24 18:39:25 +0100

    Update 6-sous-requetes.sql

--- sgbd/sqlserver/formation/ch3-extraire-donnees/6-sous-requetes.sql
+++ sgbd/sqlserver/formation/ch3-extraire-donnees/6-sous-requetes.sql
@@ -13,3 +13,26 @@ SELECT *
 	FROM CLIENTS
 	WHERE nom LIKE 'Kornu'
   );
+--
+-- quand la requête interne envoit plus d'un résultat, on utilise des opérateurs différents
+-- IN ou =ANY
+SELECT *
+  FROM ARTICLES
+  WHERE code_cat IN (SELECT code_cat
+	FROM CATEGORIES
+	WHERE libelle_cat like 'Cl%'
+  );
+-- =======================
+-- sous-requete corrélée
+-- =======================
+-- tous les articles non présents sur une ligne de commandes
+SELECT reference_art
+  FROM ARTICLES
+  WHERE NOT EXISTS(SELECT *
+    FROM LIGNES_CDE
+	WHERE LIGNES_CDE.reference_art= ARTICLES.reference_art);
+-- à chaque fois que l'on parcours un article dans la table des articles
+-- on cherche à exécuter la sous-requête
+-- La sous-requête peut renvoyer soit 0, soit 1 ou plusieurs lignes
+-- L'instruction EXISTS renvoit VRAI lorsque la requête passé en paramètre renvoit au moins une ligne
+-- Renvoit 0 lignes, EXISTS renvoie Faux et not exists renvoie VRAI
