886f2ff6b3e9dc30f634bdcfcb8ec9de041cbb20
grouault <gildasrouault@gmail.com>
2019-02-24 18:33:16 +0100

    Update 6-sous-requetes.sql

--- sgbd/sqlserver/formation/ch3-extraire-donnees/6-sous-requetes.sql
+++ sgbd/sqlserver/formation/ch3-extraire-donnees/6-sous-requetes.sql
@@ -3,4 +3,13 @@
 -- =============
 -- éviter l'utilisation de tables temporaires
 -- toutes les commandes d'un client
---
+-- ==> exécuter une requête (requête externe) en se basant sur le résultat d'une sous-requête
+-- La sous-requête (requête interne) est exécutée une seule-fois et est appelé requête imbriquée.
+-- Les requêtes sont imbriquées mais sont indépendantes.
+-- exécution de la requête interne puis la requête externe.
+SELECT *
+  FROM COMMANDES
+  WHERE numero_cli=(SELECT numero
+	FROM CLIENTS
+	WHERE nom LIKE 'Kornu'
+  );
