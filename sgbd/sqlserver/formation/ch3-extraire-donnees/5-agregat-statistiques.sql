be47baf4edfa2e67b9dc720d74c456982ce89e9f
grouault <gildasrouault@gmail.com>
2019-02-24 17:45:19 +0100

    Update 5-agregat-statistiques.sql

--- sgbd/sqlserver/formation/ch3-extraire-donnees/5-agregat-statistiques.sql
+++ sgbd/sqlserver/formation/ch3-extraire-donnees/5-agregat-statistiques.sql
@@ -1,3 +1,16 @@
 -- ===============================
 -- AGREGAT ET NIVEAU DE PRECISION
 -- ===============================
+-- afficher pour un dépôt donné, un libellé_cat, une désignation_art la somme des quantité en stock
+-- ==> pratique mais on ne sait pas au total 
+---- combien on dispose de clavier
+---- combien on a d'articles dans le dépôt de Nantes
+----
+SELECT depot, libelle_cat , designation_art ,sum(qte_stk) as total
+  FROM STOCKS stk
+  INNER JOIN ARTICLES art
+  ON stk.reference_art=art.reference_art
+  INNER JOIN CATEGORIES cat
+  ON cat.code_cat=art.code_cat
+  WHERE depot IN ('N','P1')
+  GROUP BY depot, libelle_cat , designation_art;
