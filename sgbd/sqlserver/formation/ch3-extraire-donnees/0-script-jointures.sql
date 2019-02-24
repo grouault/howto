94d32e3300fdf4d828482d0ce4ee0323bb3682fc
grouault <gildasrouault@gmail.com>
2019-02-24 17:11:54 +0100

    Update 0-script-jointures.sql

--- sgbd/sqlserver/formation/ch3-extraire-donnees/0-script-jointures.sql
+++ sgbd/sqlserver/formation/ch3-extraire-donnees/0-script-jointures.sql
@@ -130,3 +130,21 @@ SELECT TOP 5 PERCENT cde.numero_cde,
   ON lig.reference_art=art.reference_art
   GROUP BY cde.numero_cde
   ORDER BY ca DESC;  
+
+-- =======================
+-- TABLES TEMPORAIRES
+-- =======================  
+SELECT numero, nom, prenom
+  INTO CLINANTES
+  FROM CLIENTS
+  WHERE ville='Nantes';
+GO
+SELECT *
+  FROM CLINANTES;
+GO
+SELECT reference_art, sum(qte_cde) as quantite
+  INTO ##artCde
+  FROM LIGNES_CDE
+  GROUP BY reference_art;
+GO
+SELECT * FROM ##artCde;
